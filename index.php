7ef0f4fa0050da2a306e5bd1aa8b87a64d2aa20a
vipul yadav <128059254+vipulyadav911@users.noreply.github.com>
2024-09-04 22:37:23 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,216 @@
+<?php include("header.php")?>
+<?php include("banner.php")?>
+
+
+
+    <h1>Recently Played</h1>
+
+    <div class="container">
+        <div class="card">
+            <img src="https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://img-dynamic.wynk.in/unsafe/top/filters:watermark(https://discovery-prod-arsenal.s3.ap-south-1.amazonaws.com/arsenal/library/new_songs_mix_1/var4/v4/new_songs_mix_1.png,0,-0,0,100,100)/https://s3.ap-south-1.amazonaws.com/wynk-music-cms/srch_sonymusic/A10301A0005300204S_20240613032438440/1718242806711/resources/A10301A0005300204S.jpg?dynamic=true"
+                alt="Image 1">
+            <div class="card-title"></div>
+        </div>
+        <div class="card">
+            <img src="https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/discovery-prod-arsenal/arsenal/artworks/668846d2741b1d48a2201db1/COLLECTION_37382931565378.png"
+                alt="Image 2">
+            <div class="card-title"></div>
+        </div>
+        <div class="card">
+            <img src="https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/discovery-prod-arsenal/arsenal/artworks/6687ed81440a065199998599/COLLECTION_115658765291769.png"
+                alt="Image 3">
+            <div class="card-title">New English Songs</div>
+        </div>
+        <div class="card">
+            <img src="https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/wynk-music-cms/srch_timesmusic/2024-07/01/11-36/8902633356702.jpg"
+                alt="Image 4">
+            <div class="card-title">Sarfira</div>
+        </div>
+        <div class="card">
+            <img src="https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/wynk-music-cms/srch_universalmusic/00602465638707_20240517010339240/1715912408377/24UMGIM36186_T1_cvrart.jpg"
+                alt="Image 5">
+            <div class="card-title">HIT ME HARD AND SOFT</div>
+        </div>
+        <div class="card">
+            <img src="https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/wynk-music-cms/srch_wmg/20240704184744987/5021732430656/1720120228886/resources/5021732430656.jpg"
+                alt="Image 6">
+            <div class="card-title">KODAK</div>
+        </div>
+        <div class="arrow">&#10145;</div>
+    </div>
+
+    <button class="more-button">More</button>
+
+    <h2>Top Charts</h2>
+
+    <div class="container">
+        <div class="card">
+            <img src="https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/discovery-prod-arsenal/arsenal/artworks/6685139b58029731bad8ad51/COLLECTION_72396948494844.png"
+                alt="Image 7">
+            <div class="card-title">Weekly Top 20: English</div>
+        </div>
+        <div class="card">
+            <img src="https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/discovery-prod-arsenal/arsenal/artworks/668d100fc37871382925410e/COLLECTION_74806066203586.png"
+                alt="Image 8">
+            <div class="card-title">Wynk Top 100</div>
+        </div>
+        <div class="card">
+            <img src="https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/discovery-prod-arsenal/arsenal/artworks/65ae64053c0587731bbb2cc2/COLLECTION_33857135845222.png"
+                alt="Image 9">
+            <div class="card-title">Romance Top 20</div>
+        </div>
+        <div class="card">
+            <img src="https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/discovery-prod-arsenal/arsenal/artworks/66852322005e714d02792295/COLLECTION_79511443349275.png"
+                alt="Image 10">
+            <div class="card-title">Weekly Top 20</div>
+        </div>
+        <div class="card">
+            <img src="https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/discovery-prod-arsenal/arsenal/artworks/6672926f09c775014b832f09/COLLECTION_17445507226562.png"
+                alt="Image 11">
+            <div class="card-title">Romance Top 50</div>
+        </div>
+        <div class="card">
+            <img src="https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/discovery-prod-arsenal/arsenal/artworks/65a6bc638439b656247058ac/COLLECTION_50147806268119.png"
+                alt="Image 12">
+            <div class="card-title">Pop Top 20</div>
+        </div>
+        <div class="arrow">&#10145;</div>
+    </div>
+    <button class="more-button">More</button>
+    <h2>Indie Music</h2>
+
+    <div class="container">
+        <div class="card">
+            <img src="https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/discovery-prod-arsenal/arsenal/artworks/668cf59b580dd94d581405b1/COLLECTION_250654923241946.png"
+                alt="Image 7">
+            <div class="card-title"></div>
+        </div>
+        <div class="card">
+            <img src="https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/discovery-prod-arsenal/arsenal/artworks/66687db66174645c36c259fa/COLLECTION_134434431334355.png"
+                alt="Image 8">
+            <div class="card-title"></div>
+        </div>
+        <div class="card">
+            <img src="https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/discovery-prod-arsenal/arsenal/artworks/668cf8af471e2d0f68ad6112/COLLECTION_18791939141983.png"
+                alt="Image 9">
+            <div class="card-title"></div>
+        </div>
+        <div class="card">
+            <img src="https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/discovery-prod-arsenal/arsenal/artworks/65a91a8c198d706e317860b8/COLLECTION_172242987693001.png"
+                alt="Image 10">
+            <div class="card-title"></div>
+        </div>
+        <div class="card">
+            <img src="https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/discovery-prod-arsenal/arsenal/artworks/6686466689e1871e27372f7e/COLLECTION_17157950564640.png"
+                alt="Image 11">
+            <div class="card-title"></div>
+        </div>
+        <div class="card">
+            <img src="https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/discovery-prod-arsenal/arsenal/artworks/6666af959dba9258070225f2/COLLECTION_448932961242112.png"
+                alt="Image 12">
+            <div class="card-title"></div>
+        </div>
+        <div class="arrow">&#10145;</div>
+    </div>
+    <h2>Top Indie Artists</h2>
+
+    <div class="container">
+        <div class="circle-img">
+            <img src="https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/discovery-prod-zion/zion/1673244784213-Prateek_Kuhad.jpg"
+                alt="Image 1">
+        </div>
+
+        <div class="circle-img">
+            <img src="https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3-ap-south-1.amazonaws.com/wynk-music-cms/music/artists/profile/TLT%20Black.jpg"
+                alt="Image 2">
+        </div>
+
+        <div class="circle-img">
+            <img src="https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/discovery-prod-zion/zion/1696453099940-Nikhita_Gandhi.png"
+                alt="Image 3">
+        </div>
+
+        <div class="circle-img">
+            <img src="https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3-ap-south-1.amazonaws.com/wynk-music-cms/srch_wyndie/c64ec93b-9bbf-4182-ae34-25ebf99e044b/1718803877811/file-1718803854893-715814282-73838836-7D84-48CC-A5E8-2F750E135D26.png"
+                alt="Image 4">
+        </div>
+
+        <div class="circle-img">
+            <img src="https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3-ap-south-1.amazonaws.com/wynk-music-cms/music/artists/profile/5ed7758f7c0c58409e2c4317.png"
+                alt="Image 5">
+        </div>
+
+        <div class="circle-img">
+            <img src="https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3-ap-south-1.amazonaws.com/wynk-music-cms/music/artists/profile/5f58bf247c0c585d96ebbd0d.png"
+                alt="Image 6">
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="card">
+            <img src="https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/wynk-music-cms/srch_universalmusic/00602465590111_20240412074303555/1712910008040/24UMGIM32304_T1_cvrart.jpg"
+                alt="Image 7">
+            <div class="card-title"></div>
+        </div>
+        <div class="card">
+            <img src="https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3-ap-south-1.amazonaws.com/wynk-music-cms/srch_wmg/music/190295851330/1610078687/srch_wmg_A10302B0003858284M.jpg"
+                alt="Image 8">
+            <div class="card-title"></div>
+        </div>
+        <div class="card">
+            <img src="https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3-ap-south-1.amazonaws.com/wynk-music-cms/music/382519.jpg"
+                alt="Image 9">
+            <div class="card-title"></div>
+        </div>
+        <div class="card">
+            <img src="https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/wynk-music-cms/srch_universalmusic/00602465891812_20240607011726930/1717724427307/24UMGIM60796_T1_cvrart.jpg"
+                alt="Image 10">
+            <div class="card-title"></div>
+        </div>
+        <div class="card">
+            <img src="https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3-ap-south-1.amazonaws.com/wynk-music-cms/srch_universalmusic/music/update/1594327914/srch_universalmusic_00602567332756-USQ4E1703341.jpg"
+                alt="Image 11">
+            <div class="card-title"></div>
+        </div>
+        <div class="card">
+            <img src="https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/wynk-music-cms/srch_sonymusic/A10301A0005254838E_20240319015545297/1710810916230/resources/A10301A0005254838E.jpg"
+                alt="Image 12">
+            <div class="card-title"></div>
+        </div>
+        <div class="arrow">&#10145;</div>
+    </div>
+
+    <div class="container">
+        <div class="card">
+            <img src="https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/wynk-music-cms/srch_hungama/8903431984166_20240301021024/8903431984166/1709240433125/resources/8903431984166.jpg"
+                alt="Image 1">
+            <div class="card-title"></div>
+        </div>
+        <div class="card">
+            <img src="https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/wynk-music-cms/srch_timesmusic/music/1720163755011/khudaya.jpg"
+                alt="Image 2">
+            <div class="card-title"></div>
+        </div>
+        <div class="card">
+            <img src="https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/wynk-music-cms/srch_universalmusic/00602468005667_20240625092802609/1719309005174/24UMGIM69497_T1_cvrart.jpg"
+                alt="Image 3">
+            <div class="card-title">New English Songs</div>
+        </div>
+        <div class="card">
+            <img src="https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/wynk-music-cms/srch_zeemusic/2024-06/01/16-11/ZMC08401.jpg"
+                alt="Image 4">
+            <div class="card-title">Sarfira</div>
+        </div>
+        <div class="card">
+            <img src="https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/wynk-music-cms/srch_zeemusic/2024-06/18/05-06/ZMC08454.jpg"
+                alt="Image 5">
+            <div class="card-title">HIT ME HARD AND SOFT</div>
+        </div>
+        <div class="card">
+            <img src="https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/wynk-music-cms/srch_sonymusic/A10301A0005289589V_20240514174233800/1715702423500/resources/A10301A0005289589V.jpg"
+                alt="Image 6">
+            <div class="card-title">KODAK</div>
+        </div>
+        <div class="arrow">&#10145;</div>
+    </div>
+    <?php include("footer.php")?>
